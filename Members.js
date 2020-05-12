1ca3338578bc51ea8aaa367bbc5cff6181766572
Charles De Poortere <c.depoortere28@gmail.com>
2020-05-12 19:18:28 +0200

    Express crash course

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: 'Charles De Poortere',
+    email: 'charles@depoortere.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Arthur De Poortere',
+    email: 'arthur@depoortere.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Alessandro Milly',
+    email: 'boubi@milly.com',
+    status: 'active'
+  },
+  {
+    id: 4,
+    name: 'Léo Milly',
+    email: 'leo@milly.com',
+    status: 'inactive'
+  }
+];
+
+module.exports = members;
